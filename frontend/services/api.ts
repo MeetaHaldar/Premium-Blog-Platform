e18ae55bd488ddf55aab018dfb7068d831f4cbbc
MeetaHaldar <meetahaldar1001@gmail.com>
2026-05-24 23:03:11 +0530

    added backend vercel link

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "premium-blog-platform-production.up.railway.app";
+  "premium-blog-platform-production.up.railway.app" || "https://premium-blog-platform-7wt2.vercel.app/"
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: API_URL,
